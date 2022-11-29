707ce76e15c0712370c7761e22c8fe81f2df83da
Johnson <zhangs@shinetechsoftware.com>
2022-11-29 08:57:47 +0800

    单例模式

---
+++ DesignModes/Singleton/Singleton.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp1.DesignModes.Singleton;
+
+public class Singleton
+{
+    // private static Singleton uniqueInstance;
+    private static volatile Singleton uniqueInstance; //volatile解决多线程中的单例模式的问题
+
+    public Singleton() { }
+
+    public static Singleton Instance()
+    {
+        if (uniqueInstance == null)
+            uniqueInstance = new Singleton();
+
+        return uniqueInstance;
+    }
+}
